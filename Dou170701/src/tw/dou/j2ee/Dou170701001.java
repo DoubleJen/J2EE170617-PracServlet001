9f7983eb31615a44e1d148ca01ecf6a09e2599be
Chang <qoo2003654@gmail.com>
2017-07-01 11:01:54 +0800

    170701001

---
+++ Dou170701/src/tw/dou/j2ee/Dou170701001.java
@@ -0,0 +1,32 @@
+package tw.dou.j2ee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+			//路徑
+@WebServlet("/AAA01.php")
+/////////////////////////////////
+public class Dou170701001 extends HttpServlet {  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+//		System.out.println("doGet");
+/////////////////////////////////
+		
+		
+		//Hello, World
+		try (PrintWriter writer = response.getWriter()){
+			//PrintWriter的autoclose
+		writer.write("Hello, World");
+		writer.flush();
+		}
+/////////////////////////////////
+	
+	}
+
+}
