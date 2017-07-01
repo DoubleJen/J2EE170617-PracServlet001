88ac585b7ad0a6765cd186a527b4b660cad939ab
Chang <qoo2003654@gmail.com>
2017-07-01 16:05:47 +0800

    170701 002

--- Dou170701/src/tw/dou/j2ee/Dou170701007.java
+++ Dou170701/src/tw/dou/j2ee/Dou170701007.java
@@ -1,5 +1,5 @@
 package tw.dou.j2ee;
-
+//006==>007
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,25 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-			//路徑
-@WebServlet("/Dou170701001")
-/////////////////////////////////
-public class Dou170701001 extends HttpServlet {  
+/**
+ * Servlet implementation class Dou170701007
+ */
+@WebServlet("/Dou170701007")
+public class Dou170701007 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-//		System.out.println("doGet");
-/////////////////////////////////
+		int x = Integer.parseInt(request.getParameter("x"));
+		int y = Integer.parseInt(request.getParameter("y"));
+		int result = x + y;
+		System.out.println(result);
 		
-		
-		//Hello, World
 		response.setContentType("text/html; charset=UTF-8");
-		try (PrintWriter writer = response.getWriter()){
-			//PrintWriter的autoclose
-		writer.write("Hello, World");
-		writer.flush();
-		}
-/////////////////////////////////
-	
+		PrintWriter pw = response.getWriter();
+		pw.write(x + " + " + y + " = " + result);
+		pw.flush();
+		pw.close();
+		
 	}
 
+	
 }
